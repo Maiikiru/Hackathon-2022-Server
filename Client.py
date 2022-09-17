44fb713e760469a12a4c474e051e5ee91b4428e5
Maiikiru <89322078+Maiikiru@users.noreply.github.com>
2022-09-17 09:33:23 -0400

    Add files via upload

--- Client.py
+++ Client.py
@@ -1,7 +1,7 @@
 import socket
 
 IP = socket.gethostbyname(socket.gethostname()) #Todo change this later to connect to a non-same machine
-PORT = 7522
+PORT = 7523                                     #PORT 7523 for sending videos.
 ADDRESS = (IP, PORT)
 FORMAT = "utf-8"
 SIZE = 1024
@@ -15,7 +15,6 @@ def main():
 
     with open("D:\\SampleJavaPath\\tuyuUnderKids.mp4","rb") as video:
         buffer = video.read()
-        print(buffer)
         client.sendall(buffer)
 
 
--- HomeNode.py
+++ HomeNode.py
@@ -1,20 +1,15 @@
 import socket
 import os
-import threading
 
-IP = socket.gethostbyname(
-    socket.gethostname())   # Since we will be connected to the same IP address, this code will work
-                            # Upon changing to a non-local WI-FI, this will need to be changed.
-PORT = 7523                 #PORT 7523 for sending videos.
+IP = socket.gethostbyname(socket.gethostname()) # Since we will be connected to the same IP address, this code will work
+# Upon changing to a non-local WI-FI, this will need to be changed.
+PORT = 7522                                     #PORT 7522 for sending text files.
 ADDRESS = (IP, PORT)
 FORMAT = "utf-8"
 BUFFER_SIZE = 1024
-VIDEO_FILE_PATH = "D:\\SampleJavaPath\\Videos\\"
+FILE_PATH = "D:\\SampleJavaPath\Videos\\"
 
-
-
-# Sending files over TCP.
-def accept_video_server():
+def accept_text_server():
     print("STARTING SERVER")
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server.bind(ADDRESS)
@@ -28,11 +23,8 @@ def accept_video_server():
         try:
             print("Starting to read bytes..")
             buffer = client_socket.recv(BUFFER_SIZE)
-            i = 0
-            while os.path.exists(f"{VIDEO_FILE_PATH}video{i}.mp4"):
-                i += 1
 
-            with open(VIDEO_FILE_PATH + "video" + str(i) + ".mp4", "wb") as video:
+            with open(FILE_PATH + "commands.txt", "wb") as video:
                 while buffer:
                     video.write(buffer)
                     buffer = client_socket.recv(BUFFER_SIZE)
@@ -48,11 +40,5 @@ def accept_video_server():
         client_socket.close()
         print(f"Disconnected {addr} disconnected.")
 
-
-
-
-
 if __name__ == "__main__":
-    x = threading.Thread(target=accept_video_server())
-    x.start()
-
+    accept_text_server()
